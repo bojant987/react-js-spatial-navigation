96a1bfee940c1486ad8e39b8ad4f4dcdd4927941
Darkø Tasevski <darko.tasevski@redbox.com>
2018-12-05 20:58:54 +0100

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -7,16 +7,17 @@ module.exports = {
     filename: 'index.js',
     libraryTarget: 'commonjs2'
   },
+  mode: 'production',
   module: {
     rules: [
       {
         test: /\.js$/,
         include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|bower_components|build)/,
+        exclude: /(node_modules|build)/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env', 'stage-0']
+            presets: ['@babel/preset-env']
           }
         }
       }
